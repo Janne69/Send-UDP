002a9cdc0fac9d499c21046186a91fc943920742
Janne69 <jan.lohman@alten.se>
2014-02-25 11:08:03 +0100

    New contacts Page

---
+++ Send_Values/frmContacts.vb
@@ -0,0 +1,16 @@
+﻿Public Class frmContacts
+
+
+#Region " Global Variable Declaration "
+    Private bFormLoaded As Boolean
+#End Region
+
+#Region " Load/End "
+    Private Sub frmContacts_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        bFormLoaded = True
+    End Sub
+#End Region
+
+
+End Class
